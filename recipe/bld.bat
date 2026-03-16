34d0c2cdc596072a4b263710ce2ba6a346c73874
Finn Womack <finnwomack@microsoft.com>
2026-03-16 01:51:27 -0700

    add target for win_arm64

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,12 @@
 
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
+set CARGO_EXTRA_ARGS=
+if %target_platform% neq %build_platform% (
+  if %target_platform%==win-arm64 (
+    set CARGO_EXTRA_ARGS=--target aarch64-pc-windows-msvc
+  )
+)
 
 :: build
 cargo install --locked ^
@@ -9,6 +15,7 @@ cargo install --locked ^
     --path . ^
     --features pcre2 ^
     --no-track ^
+    %CARGO_EXTRA_ARGS% ^
     || exit 1
 
 :: dump licenses
